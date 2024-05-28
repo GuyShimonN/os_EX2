62ea1c6502c97ac39e839244cceb8522bfe9518f
GuyShimonN <guyguy845@gmail.com>
2024-05-28 22:28:56 +0300

    finsh 4

--- 4/mynetcat.cpp
+++ 4/mynetcat.cpp
@@ -11,10 +11,13 @@
 #include <sstream>
 #include <csignal>
 #include <sys/wait.h>
+#include <ctime>
+#include <signal.h>
 
 using namespace std;
 
 bool running = true;
+int timeout = -1;
 
 // Function to split a string by spaces into a vector of strings
 vector<string> split(const string &str)
@@ -110,7 +113,57 @@ int start_tcp_client(const string &hostname, const string &port)
         close(client_sock);
         exit(EXIT_FAILURE);
     }
-    cout << "  Connected to server " << endl;
+    cout << "Connected to server" << endl;
+    return client_sock;
+}
+
+// Function to start a UDP server
+int start_udp_server(const string &port)
+{
+    int server_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_sock < 0)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error binding socket");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    return server_sock;
+}
+
+// Function to start a UDP client
+int start_udp_client(const string &hostname, const string &port, struct sockaddr_in &server_addr)
+{
+    int client_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_sock < 0)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct hostent *server = gethostbyname(hostname.c_str());
+    if (server == nullptr)
+    {
+        cerr << "Error: No such host" << endl;
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+
     return client_sock;
 }
 
@@ -122,16 +175,25 @@ void signal_handler(int signal)
     }
 }
 
+void alarm_handler(int signal)
+{
+    if (signal == SIGALRM)
+    {
+        running = false;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    signal(SIGINT, signal_handler); // Handle Ctrl+Z to terminate the program gracefully
+    signal(SIGINT, signal_handler); // Handle Ctrl+C to terminate the program gracefully
+    signal(SIGALRM, alarm_handler); // Handle alarm signal for timeout
 
     int opt;
     char *program = nullptr;
     string input_redirect, output_redirect;
 
     // Using getopt to parse the command-line arguments
-    while ((opt = getopt(argc, argv, "e:i:o:b:")) != -1)
+    while ((opt = getopt(argc, argv, "e:i:o:b:t:")) != -1)
     {
         switch (opt)
         {
@@ -152,25 +214,34 @@ int main(int argc, char *argv[])
                 input_redirect = optarg;
                 output_redirect = optarg;
                 break;
+            case 't':
+                timeout = stoi(optarg);
+                break;
             default:
-                cerr << " first Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+                cerr << "Usage: " << argv[0]
+                     << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>] [-t <timeout>]" << endl;
                 return EXIT_FAILURE;
         }
     }
 
+    if (timeout > 0)
+    {
+        alarm(timeout);
+    }
+
     if (program)
     { // If the program name is provided, execute it with redirections if specified
-        cout << "the input was: " << input_redirect << endl;
-        cout << "the output was: " << output_redirect << endl;
+        cout << "The input was: " << input_redirect << endl;
+        cout << "The output was: " << output_redirect << endl;
 
         // Add "./" to the beginning of the program name
         string program_str = "./" + string(program);
 
         vector<string> split_program = split(program_str); // Split the program name and arguments
-        vector<char*> args;                              // Vector of char to store the arguments for execvp
+        vector<char *> args;                               // Vector of char* to store the arguments for execvp
         for (const auto &arg : split_program)
         { // Convert the arguments to char* and store in the vector
-            args.push_back(const_cast<char*>(arg.c_str()));
+            args.push_back(const_cast<char *>(arg.c_str()));
         }
         args.push_back(NULL);
 
@@ -203,7 +274,22 @@ int main(int argc, char *argv[])
                     }
                     close(server_sock);
                 }
-                // Additional input redirect implementations can be added here
+                else if (input_redirect.substr(0, 4) == "UDPS")
+                {
+                    int port = stoi(input_redirect.substr(4));
+                    int server_sock = start_udp_server(to_string(port));
+                    struct sockaddr_in client_addr = {};
+                    socklen_t client_len = sizeof(client_addr);
+                    char buffer[1024];
+                    ssize_t n = recvfrom(server_sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_len);
+                    if (n < 0)
+                    {
+                        perror("Error receiving UDP message");
+                        close(server_sock);
+                        return EXIT_FAILURE;
+                    }
+                    handle_client_input(server_sock);
+                }
             }
 
             if (!output_redirect.empty() && output_redirect != input_redirect)
@@ -239,7 +325,32 @@ int main(int argc, char *argv[])
                         return EXIT_FAILURE;
                     }
                 }
-                // Additional output redirect implementations can be added here
+                else if (output_redirect.substr(0, 4) == "UDPC")
+                {
+                    string host_port = output_redirect.substr(4);
+                    size_t comma_pos = host_port.find(',');
+                    if (comma_pos != string::npos)
+                    {
+                        string hostname = host_port.substr(0, comma_pos);
+                        string port = host_port.substr(comma_pos + 1);
+                        struct sockaddr_in server_addr;
+                        int client_sock = start_udp_client(hostname, port, server_addr);
+                        handle_client_output(client_sock);
+                        char buffer[1024];
+                        ssize_t n;
+                        while ((n = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0)
+                        {
+                            buffer[n] = '\0';
+                            sendto(client_sock, buffer, n, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+                        }
+                        close(client_sock);
+                    }
+                    else
+                    {
+                        cerr << "Invalid UDPC format. Expected UDPC<hostname,port>" << endl;
+                        return EXIT_FAILURE;
+                    }
+                }
             }
 
             // Set the buffer to be line-buffered
@@ -279,7 +390,7 @@ int main(int argc, char *argv[])
                     close(server_sock);
                     return EXIT_FAILURE;
                 }
-                cout << "the chosen port is: " << port << " and option " << argv[2] << endl;
+                cout << "The chosen port is: " << port << " and option " << argv[2] << endl;
                 cout << "Connected to client" << endl;
                 cout << "Enter message to send to client" << endl;
                 fd_set read_fds;
@@ -390,7 +501,8 @@ int main(int argc, char *argv[])
         }
         else
         {
-            cerr << "second Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+            cerr << "Usage: " << argv[0]
+                 << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>] [-t <timeout>]" << endl;
             return EXIT_FAILURE;
         }
     }
@@ -441,4 +553,13 @@ int main(int argc, char *argv[])
 
 
 
-
+// ./mync -e "ttt 123456789" -i UDPS5555
+// nc -u localhost 5555
+//
+// nc -l 5556
+// ./mync -e "ttt 123456789" -i UDPS5555 -o TCPClocalhost,5556
+// nc -u localhost 4050
+//
+//
+// nc -l 5556
+// ./mync -e "ttt 123456789" -o TCPClocalhost,5556
